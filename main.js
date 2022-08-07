e1a594678f3502139e78a2899f6527c04992f91c
kov27 <110753069+kov27@users.noreply.github.com>
2022-08-07 14:00:52 +1000

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function mineGold() {
 function buyGoldPerClick() {
     if (gameData.gold >= gameData.goldPerClickCost) {
         gameData.gold -= gameData.goldPerClickCost
-        gameData.goldPerClick *= 1
+        gameData.goldPerClick *= 2
         gameData.goldPerClickCost *= 2
         document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
         document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
